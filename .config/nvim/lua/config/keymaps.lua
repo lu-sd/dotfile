3ff303123c7afd7aa421b634ff813504503eaea5
lu-sd <luhou.me@gmail.com>
2024-08-25 13:34:29 -0700

    add keymap to leet code

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,11 +3,17 @@
 -- Add any additional keymaps here
 
 --terminal
+
 vim.keymap.set("t", "<esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
 vim.keymap.del("t", "<esc><esc>")
 vim.keymap.del("n", "<c-/>")
+vim.keymap.del("n", "<leader>l")
 vim.keymap.set("i", "<C-a>", "<C-r>0", { noremap = true, silent = true })
-
+-- leetcode
+vim.keymap.set("n", "<leader>ll", "<cmd>Leet list<cr>", { desc = "leetcode list" })
+vim.keymap.set("n", "<leader>ls", "<cmd>Leet submit<cr>", { desc = "leetcode submit" })
+vim.keymap.set("n", "<leader>lr", "<cmd>Leet run<cr>", { desc = "leetcode run" })
+--
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
 vim.keymap.set(
   "n",
